3268418baa108197d494182aa8007eefefc737d7
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-06-23 17:19:34 +0200

    Add files via upload

---
+++ data/list.py
@@ -0,0 +1,21 @@
+import requests
+from getpass import getpass
+
+endpoint = "http://localhost:8000/api/auth/"
+password = getpass()
+
+# data = {"title": "This is the title"}
+auth_response = requests.post(
+    endpoint, json={"username": "andreas", "password": "1234"}
+)
+
+if auth_response.status_code == 200:
+    token = auth_response.json()["token"]
+    print(token)
+    headers = {"Authorization": f"Token {token}"}
+    print(headers)
+
+    endpoint = "http://localhost:8000/api/products/"
+
+    get_response = requests.get(endpoint, headers=headers)
+    print(get_response.json())
