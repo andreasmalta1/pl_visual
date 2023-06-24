68d415605413386b0a2532c72427ff2db009eecb
Andreas Calleja <andreascalleja@gmail.com>
2023-06-24 18:45:36 +0200

    Bearer token

--- data/list.py
+++ data/list.py
@@ -12,7 +12,7 @@ auth_response = requests.post(
 if auth_response.status_code == 200:
     token = auth_response.json()["token"]
     print(token)
-    headers = {"Authorization": f"Token {token}"}
+    headers = {"Authorization": f"Bearer {token}"}
     print(headers)
 
     endpoint = "http://localhost:8000/api/products/"
--- pl_visuals/minutes/views.py
+++ pl_visuals/minutes/views.py
@@ -24,6 +24,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
+from api.authentication import TokenAuthentication
 from .models import Product
 from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
@@ -37,7 +38,10 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes = [authentication.SessionAuthentication]
+    authentication_classes = [
+        authentication.SessionAuthentication,
+        TokenAuthentication,
+    ]
     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
